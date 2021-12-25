130f69a58fbdacbc4070eb6ca9fd930d5c8843ad
Erfan Abdi <erfangplus@gmail.com>
2021-12-25 10:14:18 +0330

    venus: Fix pureShot_parameter xml format
    Change-Id: Ie9beed910101f2550ca573157242cd2f9c4ce140

--- extract-files.py
+++ extract-files.py
@@ -29,6 +29,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/camera/pureShot_parameter.xml': blob_fixup()
+        .regex_replace(r'=(\d+)>', r'="\1">'),
     'vendor/lib/hw/audio.primary.venus.so': blob_fixup()
         .replace_needed('/vendor/lib/liba2dpoffload.so', '/odm/lib/liba2dpoffload.so')
         .replace_needed('/vendor/lib/libssrec.so', '/odm/lib/libssrec.so')
