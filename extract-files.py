1bb74843ef2986b08af35a33db9867802509364e
LuK1337 <priv.luk@gmail.com>
2023-11-30 20:47:49 +0100

    venus: Setup firmware extraction
    Change-Id: I693e59abbfcec4f016de324517ad0e24ef565c65

--- extract-files.py
+++ extract-files.py
@@ -29,6 +29,7 @@ module = ExtractUtilsModule(
     'xiaomi',
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
 )
 
 if __name__ == '__main__':
